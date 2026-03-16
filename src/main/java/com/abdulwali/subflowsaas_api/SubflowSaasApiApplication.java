d3da471d488c8cf8679540eb605c2831323b6d8d
Abdul Wali Dal <148618788+abdulwalidal@users.noreply.github.com>
2026-03-16 22:09:39 +0500

    Initial project setup

---
+++ src/main/java/com/abdulwali/subflowsaas_api/SubflowSaasApiApplication.java
@@ -0,0 +1,13 @@
+package com.abdulwali.subflowsaas_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubflowSaasApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubflowSaasApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/abdulwali/subflowsaas_api/SubflowSaasApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abdulwali.subflowsaas_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubflowSaasApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
