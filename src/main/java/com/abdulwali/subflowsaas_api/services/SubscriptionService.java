c65bd45c2cc7a5df2bf1ed6a2cc5e11bbf111ac7
Abdul Wali Dal <148618788+abdulwalidal@users.noreply.github.com>
2026-03-28 00:59:04 +0500

    Refactor SubscriptionService to return DTOs and improve subscription retrieval

--- src/main/java/com/abdulwali/subflowsaas_api/services/SubscriptionService.java
+++ src/main/java/com/abdulwali/subflowsaas_api/services/SubscriptionService.java
@@ -2,10 +2,15 @@ package com.abdulwali.subflowsaas_api.services;
 
 
 import com.abdulwali.subflowsaas_api.model.Subscription;
+import com.abdulwali.subflowsaas_api.payload.SubscriptionDTO;
+import com.abdulwali.subflowsaas_api.payload.SubscriptionResponse;
 
 import java.util.List;
 
 public interface SubscriptionService {
-    List<Subscription> getALlSubscriptions();
-    String addSubscription (Subscription subscription);
+
+
+    SubscriptionDTO addSubscription(Subscription subscription);
+
+    SubscriptionResponse getAll();
 }
--- src/main/java/com/abdulwali/subflowsaas_api/services/SubscriptionServiceImpl.java
+++ src/main/java/com/abdulwali/subflowsaas_api/services/SubscriptionServiceImpl.java
@@ -1,7 +1,10 @@
 package com.abdulwali.subflowsaas_api.services;
 
 import com.abdulwali.subflowsaas_api.model.Subscription;
+import com.abdulwali.subflowsaas_api.payload.SubscriptionDTO;
+import com.abdulwali.subflowsaas_api.payload.SubscriptionResponse;
 import com.abdulwali.subflowsaas_api.repository.SubscriptionRepository;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,15 +15,30 @@ public class SubscriptionServiceImpl implements SubscriptionService {
 
     @Autowired
     private SubscriptionRepository subscriptionRepository;
+    @Autowired
+    private ModelMapper modelMapper;
+
+
 
     @Override
-    public List<Subscription> getALlSubscriptions() {
-        return subscriptionRepository.findAll();
+    public SubscriptionDTO addSubscription(Subscription subscription) {
+        Subscription savedSubscription =  subscriptionRepository.save(subscription);
+        return modelMapper.map(savedSubscription, SubscriptionDTO.class);
+
     }
 
     @Override
-    public String addSubscription(Subscription subscription) {
-        subscriptionRepository.save(subscription);
-        return "Your subscription is added";
+    public SubscriptionResponse getAll() {
+      List<Subscription> subscriptions = subscriptionRepository.findAll();
+      List<SubscriptionDTO> subscriptionDTOS = subscriptions.stream() // [Sub1, Sub2, Sub3] → conveyor belt → Sub1 → Sub2 → Sub3
+              // for each subscription coming down
+              //→ convert it to a SubscriptionDTO using modelMapper
+              .map(subscription -> modelMapper.map(subscription, SubscriptionDTO.class))
+              .toList(); // After every item is transformed, collect them all back into a new list.
+                         // [DTO1, // DTO2, DTO3] ← collected back into a list
+      SubscriptionResponse subscriptionResponse = new SubscriptionResponse();
+      subscriptionResponse.setContent(subscriptionDTOS);
+      return subscriptionResponse;
+
     }
 }
