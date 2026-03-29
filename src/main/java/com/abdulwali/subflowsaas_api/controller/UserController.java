f63b75a3b3535d706d20d94df67e4239e7502cf0
Abdul Wali Dal <148618788+abdulwalidal@users.noreply.github.com>
2026-03-29 21:53:40 +0500

    Fix formatting issue in SubscriptionController

--- src/main/java/com/abdulwali/subflowsaas_api/controller/UserController.java
+++ src/main/java/com/abdulwali/subflowsaas_api/controller/UserController.java
@@ -16,7 +16,7 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    @PostMapping("/add")
+    @PostMapping("/admin/add")
     public ResponseEntity<UserDTO> addUser(@RequestBody User user) {
         UserDTO userDTO = userService.addUser(user);
         return new ResponseEntity<>(userDTO, HttpStatus.CREATED);
@@ -26,8 +26,28 @@ public class UserController {
     public ResponseEntity<UserResponse> getAll() {
         UserResponse userResponse = userService.getAll();
         return new ResponseEntity<>(userResponse, HttpStatus.OK);
-
     }
+
+    @GetMapping("admin/{userId}/user")
+    public ResponseEntity<UserDTO> getUserById(Long userId) {
+      UserDTO userDTO = userService.findUserById(userId);
+        return new ResponseEntity<>(userDTO, HttpStatus.OK);
+    }
+
+    @PutMapping("/admin/{userId}")
+    public ResponseEntity<UserDTO> updateUser(@RequestBody UserDTO userDTO,
+                                              @PathVariable Long userId) {
+
+        UserDTO savedUserDTO = userService.updateUser(userDTO, userId);
+        return new ResponseEntity<>(savedUserDTO, HttpStatus.OK);
+    }
+
+    @DeleteMapping("admin/{userId}")
+    public ResponseEntity<UserDTO> deleteUser(@PathVariable Long userId) {
+        UserDTO userDTO = userService.deleteUser(userId);
+        return new ResponseEntity<>(userDTO, HttpStatus.OK);
+    }
+
 
 
 
