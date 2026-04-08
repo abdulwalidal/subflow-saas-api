9f563645de3dc0e255bc751b47f5f1b4861d06b8
Abdul Wali Dal <148618788+abdulwalidal@users.noreply.github.com>
2026-04-08 23:48:46 +0500

    Add subscription update, delete, and retrieval methods to SubscriptionController

--- src/main/java/com/abdulwali/subflowsaas_api/controller/SubscriptionController.java
+++ src/main/java/com/abdulwali/subflowsaas_api/controller/SubscriptionController.java
@@ -30,6 +30,26 @@ public class SubscriptionController {
         return new ResponseEntity<>(subscriptionResponse, HttpStatus.OK);
     }
 
+    @PutMapping("/update/{Id}")
+    public ResponseEntity<SubscriptionDTO> updateSubscription (@PathVariable Long Id,
+                                                               @RequestBody SubscriptionDTO subscriptionDTO) {
+        SubscriptionDTO savedSubscriptionDTO = subscriptionService.updateSubscription(Id, subscriptionDTO);
+        return new ResponseEntity<>(savedSubscriptionDTO, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/delete{Id}")
+    public ResponseEntity<SubscriptionDTO> deleteSubscription (@PathVariable Long Id) {
+        SubscriptionDTO savedSubscriptionDTO = subscriptionService.deleteSubscription(Id);
+        return new ResponseEntity<>(savedSubscriptionDTO, HttpStatus.OK);
+    }
+
+    @GetMapping("/sub/{Id}")
+    public ResponseEntity<SubscriptionDTO> getSubscriptionById(Long Id) {
+        SubscriptionDTO savedSubscriptionDTO = subscriptionService.findSubscriptionById(Id);
+        return new ResponseEntity<>(savedSubscriptionDTO, HttpStatus.OK);
+
+    }
+
 
 
 
